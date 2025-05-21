df7195f8d9c3cc7b16749245068dedf562ced82d
jvksenia <111875399+jvksenia@users.noreply.github.com>
2025-05-21 14:35:48 +0300

    Add files via upload

---
+++ js-info.js
@@ -0,0 +1,10 @@
+alert("Кто разработал язык программирования JavaScript?");
+console.log('Язык программирования JavaScript разработал Брэндан Эйх в 1995 году, работая в компании Netscape Communications Corporation.')
+alert("За сколько дней был разработан JavaScript?");
+console.log('Брэндан Эйх создал язык за десять дней в мае 1995 года')
+alert("В каком году появился JavaScript?");
+console.log('JavaScript появился в 1995 году.')
+alert("JavaScript — это то же самое, что и Java? (отвечайте либо «JavaScript — это Java», либо «JavaScript — это не Java»)");
+console.log('JavaScript и Java — это разные языки программирования, которые решают различные задачи.')
+alert("Как называется спецификация JavaScript?");
+console.log('Спецификация для JavaScript называется ECMAScript. ')
